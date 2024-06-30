591e783d5192d940c6abc5f07f0637ca64efbc04
Shirsendu <palshirsendu03@gmail.com>
2024-06-30 12:48:03 +0530

    Fixed minor bugs

--- app.py
+++ app.py
@@ -15,6 +15,15 @@ file = st.file_uploader('Image Upload: ', ["png", "jpg", "jpeg"], accept_multipl
 if file is not None:
     try:
         img = cv2.imread(file)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        HEIGHT, WIDTH = img.shape 
+        COMPH, COMPW = int(512 * (HEIGHT / WIDTH)), 512
+
+        scaled_img = cv2.resize(img, (COMPW, COMPH))
+
+        st.image(scaled_img, caption='Original Image')
+
+        cmp = Compress(scaled_img)
 
     except Exception as e: 
         st.error(f"Error: {e}")
